284a3ad94ffb68a8f3e1c251510c39834d8e4cd3
Anup Kakde <prajyotkakde2410@gmail.com>
2022-08-26 12:55:26 +0530

    First Commit...

---
+++ src/anup_V8/Leap_Year.java
@@ -0,0 +1,19 @@
+package anup_V8;
+
+public class Leap_Year {
+
+	public static void main(String[] args) {
+		
+			int a =2023, b=a%4;
+			
+			if(b==0)
+			{
+				System.out.println("This is Leap Year");
+			}
+			else
+			{
+				System.out.println("This is Not Leap Year");
+			}
+	}
+
+	}
